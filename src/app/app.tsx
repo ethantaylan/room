ee7def18d2fd8b29309384777955139279d20c4f
Ethan <ethtaylan@gmail.com>
2023-07-02 20:39:51 +0200

    fastpush

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,22 +4,17 @@ import { LesSalles } from './pages/les-salles';
 import { AboutUs } from './pages/about-us';
 import { Contact } from './pages/contact';
 import { NotFound } from './pages/notFound';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 export const App = () => {
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/accueil" element={<Home />} />
-          <Route path="/les-salles" element={<LesSalles />} />
-          <Route path="/qui-sommes-nous" element={<AboutUs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/accueil" element={<Home />} />
+        <Route path="/les-salles" element={<LesSalles />} />
+        <Route path="/qui-sommes-nous" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
